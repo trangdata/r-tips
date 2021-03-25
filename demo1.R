005126d203ab68079d3b277384e6c61cc1e3a23e
lelaboratoire <grixor@gmail.com>
2021-03-25 13:59:14 -0400

    minor edit

--- demo1.R
+++ demo1.R
@@ -8,21 +8,23 @@ library(ggplot2)
 dat <- mtcars %>% 
   select(mpg, cyl) %>% 
   rownames_to_column('id') %>% 
-  # slice(3) %>% 
+  slice(1:3) %>% 
   {.}
 
 # Cmd+click on variable name to View()
 
 mtcars %>% 
   ggplot(aes(x = mpg, y = cyl)) +
-  # geom_point() +
-  theme_minimal() +
+  geom_point() +
+  # theme_minimal() +
   NULL
 
-# Make functions out of existing lines: Cmd+Opt+X
-add_fruits <- function(pineapples, bananas) {
-  pineapples + bananas
-  pineapples*2
+pineapples
+
+
+add_fruits <- function(apples, bananas) {
+  apples + bananas
+  apples*2
 }
 
 # Rename variable within scope: Cmd+Opt+Shift+M
